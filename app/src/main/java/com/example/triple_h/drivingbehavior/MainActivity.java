c1ff2e21f3ccee48a8bab95e480f7f9c973354b7
Triple <673294603@qq.com>
2015-11-03 00:20:32 +0800

    增加百度Android定位，重写定位部分；新增登陆后在主界面时按后退会退出程序；增加页面刷新按钮；增加在应用运行时屏幕不熄灭。

--- app/src/main/java/com/example/triple_h/drivingbehavior/MainActivity.java
+++ app/src/main/java/com/example/triple_h/drivingbehavior/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.triple_h.drivingbehavior;
 
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Handler;
+import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,113 +13,110 @@ import android.view.MenuItem;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.location.Poi;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webview;
-    LocationManager lm;
     private int TIME=1000;
+    //声明Baidu定位
+    public LocationClient mLocationClient = null;
+    public BDLocationListener myListener = new MyLocationListener();
+    //声明定时器的Handler
+    Handler handler = new Handler();
+    //Latitude纬度
+    double CurLatitude;
+    //Lontitude经度
+    double CurLontitude;
+    //速度
+    float speed;
+    //方向
+    float driection;
+    PowerManager powerManager = null;
+    PowerManager.WakeLock wakeLock = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // setContentView(R.layout.activity_main);
+        /*
+        *创建Baidu定位
+        */
+        mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
+        mLocationClient.registerLocationListener(myListener);    //注册监听函数
+        initLocation();
+        mLocationClient.start();
+        //创建WebView
         webview = new WebView(this);
         //设置WebView属性,能够执行JavaScript脚本
         webview.getSettings().setJavaScriptEnabled(true);
-        webview.setWebChromeClient(new WebChromeClient());
+        //设置网页的标题为标题栏的标题
+        WebChromeClient wvcc = new WebChromeClient() {
+                      @Override
+                        public void onReceivedTitle(WebView view, String title) {
+                             super.onReceivedTitle(view, title);
+                           Log.d("ANDROID_LAB", "TITLE=" + title);
+                          MainActivity.this.setTitle(title);
+                        }
+        };
+        //应用webview的设置
+        webview.setWebChromeClient(wvcc);
+        //打开网页
         try {
             //打开页面地址
             webview.loadUrl("http://192.168.1.194:8080/DrivingBehavior/");
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+        //允许网页重定向
         webview.setWebViewClient(new WebViewClient() {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
                 return true;
             }
-
         });
-
         setContentView(webview);
-        //获取系统LocationManager对象
-        lm=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        //从GPS获取最近的定位信息
-        Location location=lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        updateView(location);
-        //设置每3秒获取一次GPS信息
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 8, new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                updateView(location);
-            }
+        powerManager = (PowerManager)this.getSystemService(this.POWER_SERVICE);
+        wakeLock = this.powerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "My Lock");
 
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-                updateView(null);
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-                updateView(lm.getLastKnownLocation(provider));
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-                updateView(lm.getLastKnownLocation(provider));
-            }
-        });
         handler.postDelayed(runnable, TIME); //每隔1s执行
     }
-    Handler handler = new Handler();
     Runnable runnable = new Runnable()
     {
         @Override
         public void run() {
-                // handler自带方法实现定时器
-               try
-               {
-                   handler.postDelayed(this, TIME);
-                   webview.loadUrl("javascript:theLocation(" + 106.29637269 + "," + 29.59953865 + ")");
-                   System.out.println("do...");
-                 }
-               catch (Exception e) {
-                 e.printStackTrace();
-                 System.out.println("exception...");
-              }
-                }
-          };
-
-    public void updateView(Location newlocation)
-    {
-        if(newlocation!=null)
-        {
-            //webview.loadUrl("javascript:theLocation(" + newlocation.getLongitude() + "," + newlocation.getLatitude() + ")");
-            //String a=String.valueOf(newlocation.getLongitude())+"  ,  "+newlocation.getLatitude();
-            //Log.i("CDH", a+webview.getUrl());
-            /*StringBuilder sb=new StringBuilder();
-            sb.append("实时的位置信息： \n");
-            sb.append("经度： ");
-            sb.append(newlocation.getLongitude());
-            sb.append("\n纬度： ");
-            sb.append(newlocation.getLatitude());
-            sb.append("\n高度： ");
-            sb.append(newlocation.getAltitude());
-            sb.append("\n速度： ");
-            sb.append(newlocation.getSpeed());
-            sb.append("\n方向： ");
-            sb.append(newlocation.getBearing());
-            Show.setText(sb.toString());*/
-
+            // handler自带方法实现定时器
+            try
+            {
+                //handler.postDelayed(this, TIME);
+                webview.loadUrl("javascript:theLocation(" + CurLontitude + "," + CurLatitude + ")");
+                webview.loadUrl("javascript:SetSpeedAndDirection(" + speed + "," + driection + ")");
+                Log.i("BaiduLocation", "成功调用"+CurLatitude+",  "+CurLontitude);
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+                System.out.println("exception...");
+            }
+            handler.postDelayed(this, TIME);
         }
-        else
-        {
-            Log.i("CDH", "无法定位");
-        }
-    }
+    };
     @Override
     public boolean onKeyDown(int keyCoder, KeyEvent event) {
         if ((keyCoder == KeyEvent.KEYCODE_BACK) && webview.canGoBack()) {
-            webview.goBack();
+            if(webview.getUrl().startsWith("http://192.168.1.194:8080/DrivingBehavior/demo/hello"))
+            {
+                System.exit(0);
+            }
+            else
+            {
+                webview.goBack();
+            }
             return true;
         }
         return super.onKeyDown(keyCoder, event);
@@ -132,6 +127,115 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                webview.reload();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    private void initLocation(){
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
+        );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+        int span=1000;
+        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);//可选，默认false，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤gps仿真结果，默认需要
+        mLocationClient.setLocOption(option);
+    }
+    public class MyLocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //Receive Location
+            StringBuilder sb = new StringBuilder(256);
+            sb.append("time : ");
+            sb.append(location.getTime());
+            sb.append("\nerror code : ");
+            sb.append(location.getLocType());
+            sb.append("\nlatitude : ");
+            sb.append(location.getLatitude());
+            CurLatitude=location.getLatitude();
+            sb.append("\nlontitude : ");
+            sb.append(location.getLongitude());
+            CurLontitude=location.getLongitude();
+            sb.append("\nradius : ");
+            sb.append(location.getRadius());
+            if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+                sb.append("\nspeed : ");
+                sb.append(location.getSpeed());// 单位：公里每小时
+                speed=location.getSpeed();
+                sb.append("\nsatellite : ");
+                sb.append(location.getSatelliteNumber());
+                sb.append("\nheight : ");
+                sb.append(location.getAltitude());// 单位：米
+                sb.append("\ndirection : ");
+                sb.append(location.getDirection());// 单位度
+                driection=location.getDirection();
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+                sb.append("\ndescribe : ");
+                sb.append("gps定位成功");
+
+            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+                //运营商信息
+                sb.append("\noperationers : ");
+                sb.append(location.getOperators());
+                sb.append("\ndescribe : ");
+                sb.append("网络定位成功");
+            } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+                sb.append("\ndescribe : ");
+                sb.append("离线定位成功，离线定位结果也是有效的");
+            } else if (location.getLocType() == BDLocation.TypeServerError) {
+                sb.append("\ndescribe : ");
+                sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+            } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                sb.append("\ndescribe : ");
+                sb.append("网络不同导致定位失败，请检查网络是否通畅");
+            } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                sb.append("\ndescribe : ");
+                sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+            }
+            sb.append("\nlocationdescribe : ");
+            sb.append(location.getLocationDescribe());// 位置语义化信息
+            List<Poi> list =location.getPoiList();// POI数据
+            if (list != null) {
+                sb.append("\npoilist size = : ");
+                sb.append(list.size());
+                for (Poi p : list) {
+                    sb.append("\npoi= : ");
+                    sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
+                }
+            }
+            Log.i("BaiduLocationApiDem", sb.toString());
+        }
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        wakeLock.acquire();
+     }
+
+    @Override
+    protected void onPause()
+    {
+         super.onPause();
+        wakeLock.release();
     }
 
 }
